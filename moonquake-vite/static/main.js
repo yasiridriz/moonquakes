bb60deb4a86b822ed88c2b6b6464bcb1eba7e13c
naleeyyy <kahmeti123@gmail.com>
2022-10-01 14:24:38 +0200

    fixed moon and better textures

--- moonquake-vite/static/main.js
+++ moonquake-vite/static/main.js
@@ -17,15 +17,21 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const light = new THREE.PointLight( 0xffffff, 5, 100 );
-light.position.set( 2, 4, 5 );
+light.position.set( 43, 24, 35 );
 light.castShadow = true; // default false
-light.intensity = 0.6
+light.intensity = 2
 scene.add( light );
 
 light.shadow.mapSize.width = window.innerWidth; // default
 light.shadow.mapSize.height = window.innerHeight; // default
 light.shadow.camera.near = 1; // default
 light.shadow.camera.far = 1000; // default
+
+const light2 = new THREE.PointLight( 0xffffff, 5, 100 );
+light2.position.set( -43, -24, 35 );
+// light2.castShadow = true; // default false
+light2.intensity = 0.2
+scene.add( light2 );
 
 window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -36,9 +42,12 @@ window.addEventListener('resize', () => {
 const moonTexture = new THREE.TextureLoader().load('./static/models/moon.jpg')
 const moonNormal = new THREE.TextureLoader().load('./static/models/normal.jpg')
 
-const geometry = new THREE.SphereGeometry( 1, 32, 32 );
+const geometry = new THREE.SphereGeometry( 13, 32, 32 );
 
-const material = new THREE.MeshStandardMaterial( { map: moonTexture, displacementMap: moonNormal } );
+const material = new THREE.MeshStandardMaterial( { 
+    map: moonTexture, 
+    displacementMap: moonNormal
+} );
 
 const sphere = new THREE.Mesh( geometry, material );
 
@@ -47,7 +56,7 @@ sphere.receiveShadow = true;
 
 scene.add( sphere );
 
-camera.position.z = 5; // <- New code
+camera.position.z = 25; // <- New code
 
 function addStar() {
 		const g = new THREE.SphereGeometry(0.25, 24, 24);

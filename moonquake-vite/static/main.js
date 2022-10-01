2cb41b46c41a8495fca1b300bdf571914dab7421
Endrit <endritshaqiri61@gmail.com>
2022-10-01 14:50:33 +0200

    rotate added

--- moonquake-vite/static/main.js
+++ moonquake-vite/static/main.js
@@ -1,5 +1,5 @@
 import * as THREE from '../node_modules/three/build/three.module.js';
-
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
@@ -16,23 +16,11 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
+const light = new THREE.AmbientLight(0xffffff);
+scene.add(light);
 
-const light = new THREE.PointLight( 0xffffff, 5, 100 );
-light.position.set( 43, 24, 35 );
-light.castShadow = true; // default false
-light.intensity = 2
-scene.add( light );
 
-light.shadow.mapSize.width = window.innerWidth; // default
-light.shadow.mapSize.height = window.innerHeight; // default
-light.shadow.camera.near = 1; // default
-light.shadow.camera.far = 1000; // default
-
-const light2 = new THREE.PointLight( 0xffffff, 5, 100 );
-light2.position.set( -43, -24, 35 );
 // light2.castShadow = true; // default false
-light2.intensity = 0.2
-scene.add( light2 );
 
 window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -43,7 +31,7 @@ window.addEventListener('resize', () => {
 const moonTexture = new THREE.TextureLoader().load('./static/models/moon.jpg')
 const moonNormal = new THREE.TextureLoader().load('./static/models/normal.jpg')
 
-const geometry = new THREE.SphereGeometry( 13, 32, 32 );
+const geometry = new THREE.SphereGeometry( 13, 45, 32 );
 
 const material = new THREE.MeshStandardMaterial( { 
     map: moonTexture, 
@@ -58,11 +46,6 @@ scene.add( sphere );
 
 camera.position.z = 25; // <- New code
 
-const starPointLight = new THREE.PointLight(0xffffff, 15, 100);
-starPointLight.position.set(43 , 24, -10 );
-// light2.castShadow = true; // default false
-starPointLight.intensity = 5
-scene.add( starPointLight );
 function addStar() {
 		const g = new THREE.SphereGeometry(0.1, 24, 24);
 		const m = new THREE.MeshStandardMaterial( { color: 0xffffff } );
@@ -73,12 +56,13 @@ function addStar() {
 		scene.add(star);
 }
 Array(300).fill().forEach(addStar);
-
+const controls = new OrbitControls(camera, renderer.domElement);
 const rendering = function() {
     requestAnimationFrame(rendering);
 
     // Constantly rotate box
 	sphere.rotation.y += 0.002;
+		controls.update();
     renderer.render(scene, camera);
 }
 

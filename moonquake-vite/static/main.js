42a18f25af334e111795cd71ea97f9e036a64891
Endrit <endritshaqiri61@gmail.com>
2022-10-01 14:26:30 +0200

    sad

--- moonquake-vite/static/main.js
+++ moonquake-vite/static/main.js
@@ -16,10 +16,10 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
-const light = new THREE.PointLight( 0xffffff, 5, 100 );
-light.position.set( 2, 4, 5 );
+const light = new THREE.PointLight( 0xffffff, 10, 100 );
+light.position.set( 6, 4, 9 );
 light.castShadow = true; // default false
-light.intensity = 0.6
+light.intensity = 1.5
 scene.add( light );
 
 light.shadow.mapSize.width = window.innerWidth; // default
@@ -36,28 +36,28 @@ window.addEventListener('resize', () => {
 const moonTexture = new THREE.TextureLoader().load('./static/models/moon.jpg')
 const moonNormal = new THREE.TextureLoader().load('./static/models/normal.jpg')
 
-const geometry = new THREE.SphereGeometry( 1, 32, 32 );
+const geometry = new THREE.SphereGeometry( 5, 32, 32 );
 
 const material = new THREE.MeshStandardMaterial( { map: moonTexture, displacementMap: moonNormal } );
 
 const sphere = new THREE.Mesh( geometry, material );
 
 sphere.castShadow = true;
-sphere.receiveShadow = true;
 
 scene.add( sphere );
 
-camera.position.z = 5; // <- New code
+camera.position.z = 15; // <- New code
 
 function addStar() {
-		const g = new THREE.SphereGeometry(0.25, 24, 24);
+		const g = new THREE.SphereGeometry(0.05, 24, 24);
 		const m = new THREE.MeshStandardMaterial( { color: 0xffffff } );
 		const star = new THREE.Mesh(g, m);
-		const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ) );
+		const z = -15;
+		const [x, y] = Array(2).fill().map(() => THREE.MathUtils.randFloatSpread( 70 ) );
 		star.position.set(x, y, z);
 		scene.add(star);
 }
-Array(200).fill().forEach(addStar);
+Array(500).fill().forEach(addStar);
 
 const rendering = function() {
     requestAnimationFrame(rendering);
